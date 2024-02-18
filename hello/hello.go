7c49f2fe2171b27d144d00734d816c8f146b61e1
Lucas Batista <lucasjsbatista@gmail.com>
2024-02-18 13:37:04 -0400

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Lucas")
-	fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message1, err1 := greetings.Hello("Lucas")
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	fmt.Println(message1)
+
+	message2, err2 := greetings.Hello("")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println(message2)
 }
